4a12254d7fbc2a4acd9d61f5d38128afe73ef11d
Fabricio Flores <fabrifloresg@gmail.com>
2021-09-30 15:29:53 -0500

    first commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../constants';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [],
+})
+export class AuthModule {}
